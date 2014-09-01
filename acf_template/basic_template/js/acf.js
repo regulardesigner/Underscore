7fa1207a519ef6e856c8906e0d9f8934b7c67e5e
Damien Tailhades <damientailhades@MacBook-Pro-de-Damien.local>
2014-09-01 13:49:51 +0200

    adding preloader function

--- acf_template/basic_template/js/acf.js
+++ acf_template/basic_template/js/acf.js
@@ -3,8 +3,12 @@ window.addEventListener("resize", resize);
 function loaderLineHeight(){
 	document.getElementById('acf-loader').style.lineHeight = window.innerHeight+"px";
 }
+function preloader(){
+	console.log("preloader");
+}
 function init(){
 	loaderLineHeight();
+	preloader();
 }
 function resize(){
 	loaderLineHeight();
