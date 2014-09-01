f2adf0dbc73b658a7722f4929da69a4302d9b40c
Damien Tailhades <damientailhades@MacBook-Pro-de-Damien.local>
2014-09-01 11:48:01 +0200

    adding resize function

--- acf_template/basic_template/js/acf.js
+++ acf_template/basic_template/js/acf.js
@@ -1,7 +1,11 @@
 //document.getElementById("acf-loader").style.lineHeight=window.innerHeight+"px";
+window.addEventListener("resize", resize);
 function loaderLineHeight(){
 	document.getElementById('acf-loader').style.lineHeight = window.innerHeight+"px";
 }
 function init(){
+	loaderLineHeight();
+}
+function resize(){
 	loaderLineHeight();
 }
