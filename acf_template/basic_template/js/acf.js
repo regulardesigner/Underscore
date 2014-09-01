4776cd9e1e3d56c78832861760967e09559cc98a
Damien Tailhades <damientailhades@MacBook-Pro-de-Damien.local>
2014-09-01 11:38:55 +0200

    adding dynamic line-height style on the spinner
    adding a javascript function to center vertically the spinner on the
    loading page. This function add the line-height style dynamically on
    the #acf-loader div.

---
+++ acf_template/basic_template/js/acf.js
@@ -0,0 +1,7 @@
+//document.getElementById("acf-loader").style.lineHeight=window.innerHeight+"px";
+function loaderLineHeight(){
+	document.getElementById('acf-loader').style.lineHeight = window.innerHeight+"px";
+}
+function init(){
+	loaderLineHeight();
+}
